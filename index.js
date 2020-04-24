86bcc8b38ce17256aad9c766428f82157378a60f
Matheus Barbosa <mattbarb93@gmail.com>
2020-04-24 16:27:24 -0400

    added a basic HTML form through the res.send function

--- index.js
+++ index.js
@@ -5,9 +5,22 @@ const app = express();
 //Setting up route handler. When receiving a network request from the browser. Req is the incoming request from the browser into our server. Res is the outgoing response from our server, back to the browser.
 
 //If you wanna receive something, use req. If you wanna send something or communicate with the user,use res.
+
+
+//Displaying some HTML on the page. 
 app.get('/', (req, res) => {
-    //We will send a greeting to whoever made a request to server
-    res.send('hi there');
+    
+    res.send(`
+    <div>
+        <form>
+            <input placeholder="email" />
+            <input placeholder="password" />
+            <input placeholder="password confirmation" />
+            <button>Sign Up</button>
+        </form>
+    </div>
+    
+    `);
 });
 
 //Application will start listening to any incoming requests
