34e3a9290504da8979315d5ce74d18d017bd28cd
Matheus Barbosa <mattbarb93@gmail.com>
2020-04-24 15:59:05 -0400

    adding express

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hi there');
+const express = require('express');
+
+const app = express();
+
+//Setting up route handler. When receiving a network request from the browser. Req is the incoming request from the browser into our server. Res is the outgoing response from our server, back to the browser.
+
+//If you wanna receive something, use req. If you wanna send something or communicate with the user,use res.
+app.get('/', (req, res) => {
+    //We will send a greeting to whoever made a request to server
+    res.send('hi there');
+});
+
+//Application will start listening to any incoming requests
+
+app.listen(3000, () => {
+    console.log('listening');
+})
