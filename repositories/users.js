c0be07a44cb30377af54967d4eb023b0705ed007
Matheus Barbosa <mattbarb93@gmail.com>
2020-04-29 13:19:40 -0400

    added logic to delete users

--- repositories/users.js
+++ repositories/users.js
@@ -75,13 +75,23 @@ class UsersRepository {
         return records.find(record => record.id === id);
     }
 
+    async delete(id) {
+        const records = await this.getAll();
+
+        //For each record, return TRUE if it's not ID that you passed.
+        const filteredRecords = records.filter(record => record.id !== id);
+
+        //Now, pass it back to the records file
+        await this.writeAll(filteredRecords);
+    }
+
 }
 
 const test = async() => {
     const repo = new UsersRepository('users.json');
 
     
-    const user = await repo.getOne('alkdlas');
+    const user = await repo.delete('aa368593');
 
     console.log(user);
 }
