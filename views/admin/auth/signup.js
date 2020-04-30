c99a2178a8d5bdc2b899ae0db97e16634a23b64a
Matheus Barbosa <mattbarb93@gmail.com>
2020-04-30 14:02:24 -0400

    refactoring: created the layout file, so we have basic html that will be constantly used on every page

--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,7 +1,9 @@
+const layout = require('../layout');
+
 module.exports = ({ req }) => {
-    return `
+    return layout({content: `
     <div>
-        Your id is: ${req.session.userId}
+    Your id is: ${req.session.userId}
         <form method="POST">
             <input name="email" placeholder="email" />
             <input name="password" placeholder="password" />
@@ -9,5 +11,5 @@ module.exports = ({ req }) => {
             <button>Sign Up</button>
         </form>
     </div>
-    `;
+` });
 }
