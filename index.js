49a8277e32dc3ceb428b0a2bff43440f461d5d16
Matheus Barbosa <mattbarb93@gmail.com>
2020-04-29 20:35:06 -0400

    added functionality for signing in and signing out. Please refer to these when using it to a different project

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const usersRepo = require('./repositories/users');
 const app = express();
 
 //Syntax for bodyParser.urlencoded: used specifically to handle an HTML form. Will take the information from the user, parse it, and send it to our server. Set it like this so its done globally
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({
     keys: ['nqoidnqpiwdnqpdqwpo'] //Used to encrypt the user's cookie since it's a random string
 }));
 
 
-app.get('/', (req, res) => {
+app.get('/signup', (req, res) => {
 
     //Adding method="POST". Its associated with creating a record of some kind, like an user account!
     res.send(`
@@ -31,11 +31,11 @@ app.get('/', (req, res) => {
 
 
 
-app.post('/', async (req, res) => {
+app.post('/signup', async (req, res) => {
     const { email, password, passwordConfirmation } = req.body;
 
     //Input validation
-    const existingUser = await usersRepo.getOneBy({email});
+    const existingUser = await usersRepo.getOneBy({ email });
     if (existingUser) {
         return res.send('Email in use');
     }
@@ -43,16 +43,52 @@ app.post('/', async (req, res) => {
     if (password !== passwordConfirmation) {
         return res.send('Passwords must match!')
     }
-    
+
     // Create a user in our user repo to represent this person
 
-    const user = await usersRepo.create({email, password});
+    const user = await usersRepo.create({ email, password });
 
     //Store the ID of that user inside the user's cookies
     req.session.userId = user.id //Added by Cookie Session!
 
 
     res.send('Account created!');
+});
+
+app.get('/signout', (req, res) => {
+    req.session = null;
+    res.send('You are logged out!')
+});
+
+app.get('/signin', (req, res) => {
+    res.send(`
+    <div>
+        <form method="POST">
+            <input name="email" placeholder="email" />
+            <input name="password" placeholder="password" />
+            <button>Sign In</button>
+        </form>
+    </div>
+    `)
+});
+
+app.post('/signin', async (req,res) => {
+    const {email, password} = req.body;
+
+    const user = await usersRepo.getOneBy({email})
+
+    if(!user) {
+        return res.send('Email not found!');
+    }
+
+    if (user.password !== password) {
+        return res.send('Incorrect password!')
+    }
+
+    req.session.userId = user.id;
+
+    res.send('You are signed in!');
+
 });
 
 app.listen(3000, () => {
