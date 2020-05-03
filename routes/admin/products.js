4038aa992e3f0fbb1a73f84113239a36fe2dbd66
Matheus Barbosa <mattbarb93@gmail.com>
2020-05-02 22:47:11 -0400

    editing functionality added

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -6,6 +6,7 @@ const { handleErrors, requireAuth } = require('./middleware');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const productsIndexTemplate = require('../../views/admin/products/index');
+const productsEditTemplate = require('../../views/admin/products/edit');
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() }); //middleware function
@@ -50,6 +51,36 @@ router.post(
         await productsRepo.create({ title, price, image });
 
         res.redirect('/admin/products');
-    })
+    });
+
+router.get('/admin/products/:id/edit', requireAuth, async (req, res) => {
+    const product = await productsRepo.getOne(req.params.id);
+
+    if (!product) {
+        return res.send('Product not found!');
+    }
+
+    res.send(productsEditTemplate({product}))
+})
+
+router.post('/admin/products/:id/edit', 
+    requireAuth, 
+    upload.single('image'),
+    [requireTitle, requirePrice],
+    handleErrors(productsNewTemplate),
+    async (req, res) => {
+        const changes = req.body;
+
+        if(req.file) {
+            changes.image = req.file.buffer.toString('base64');
+        }
+
+        try {await productsRepo.update(req.params.id, changes)
+        } catch (err) {
+            return res.send('Could not find item!');
+        }
+
+        res.redirect('/admin/products');
+});
 
 module.exports = router;
