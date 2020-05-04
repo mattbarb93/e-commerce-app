5865f9523a7cbfe4c1f06da478ae679f847eee68
Matheus Barbosa <mattbarb93@gmail.com>
2020-05-04 15:28:12 -0400

    set up boilerplate for carts repo, so eventually we can add, delete or edit a product in the cart

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
-const adminProductsRouter = require('./routes/admin/products')
-const productsRouter = require('./routes/products')
+const adminProductsRouter = require('./routes/admin/products');
+const productsRouter = require('./routes/products');
+const cartsRouter = require('./routes/carts');
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.use(cookieSession({
 app.use(authRouter);
 app.use(adminProductsRouter);
 app.use(productsRouter);
+app.use(cartsRouter);
 
 
 
---
+++ routes/carts.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+// Receive a post request to add an item
+
+//Receive a GET request to show all items in the cart
+
+//Receive a post request to delete an item
+
+module.exports = router;
