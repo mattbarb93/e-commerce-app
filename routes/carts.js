77fd5e16144e8439fffe0a4b88117a9863d537cf
Matheus Barbosa <mattbarb93@gmail.com>
2020-05-04 15:37:11 -0400

    added post request when adding a product to cart

--- routes/carts.js
+++ routes/carts.js
@@ -3,6 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 // Receive a post request to add an item
+router.post('/cart/products', (req, res) => {
+    console.log(req.body.productId);
+
+    res.send('Product added to cart');
+})
 
 //Receive a GET request to show all items in the cart
 
